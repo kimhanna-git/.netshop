15b798c81eeed4c8cbebe527362d452b834dac0d
KIM HANNA <kimhanna.contact@gmail.com>
2022-04-11 18:49:23 +0900

    connection string added

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,24 +13,28 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 using System;
-
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
