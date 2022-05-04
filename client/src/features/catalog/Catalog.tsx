73c354b6639ad0463d10742e8fcd2de3581f2f47
KIM HANNA <kimhanna.contact@gmail.com>
2022-05-05 00:02:03 +0900

    destructuring

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -7,17 +7,17 @@ interface Props {
     
 }
 
-export default function Catalog(props: Props) {
+export default function Catalog({products, addProduct}: Props) {
     return (
         <>
             <ul>
         
-                {props.products.map(product => (
+                {products.map(product => (
                 <li key={product.id}>{product.name} - {product.price}</li>
 
                 ))}
             </ul>
-            <button onClick={props.addProduct}>Add Product</button>
+            <button onClick={addProduct}>Add Product</button>
             
         </>
         
