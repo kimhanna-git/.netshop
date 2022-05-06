b5688eabe98bdf5844a6a192ed529c2434e63780
KIM HANNA <kimhanna.contact@gmail.com>
2022-05-06 23:43:42 +0900

    grid display

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,19 +1,49 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { ListItem, ListItemAvatar, Avatar, ListItemText, CardActionArea, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
+import { makeStyles } from '@mui/styles';
+
 
 interface Props {
     product: Product;
 }
+const useStyles = makeStyles({
+    root: {
+      
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 export default function ProductCard({product}: Props) {
+    const classes = useStyles();
+
     return (
-        <ListItem key={product.id}>
-                    <ListItemAvatar>
-                        <Avatar src={product.pictureUrl} />
-                    </ListItemAvatar>
-                    <ListItemText>
-                        {product.name} - {product.price}
-                    </ListItemText>
-                </ListItem>
+        <Card className={classes.root}>
+      ``<CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="http://picsum.photos/200"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
     )
 }
