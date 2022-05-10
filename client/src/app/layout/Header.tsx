2703df159148db3ca999a0815659de7475bb644e
KIM HANNA <kimhanna.contact@gmail.com>
2022-05-10 22:15:48 +0900

    Header.tsx title error

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,9 +1,22 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 
 interface Props {
     darkMode : boolean;
     handleThemeChange: () => void;
 }
+
+const midLinks = [
+    
+    {title: 'catalog', path: '/catalog'},
+    {about: 'about', path: '/about'},
+    {contact: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    
+    {title: 'login', path: '/login'},
+    {about: 'register', path: '/register'},
+]
 
 export default function Header({darkMode, handleThemeChange}: Props) {
     return (
@@ -13,6 +26,16 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                     .NETSHOP
                 </Typography>
                 <Switch checked={darkMode} onChange={handleThemeChange} />
+                <List>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem
+
+                        >
+                            {title.toUpperCase()}
+                            
+                        </ListItem>
+                    ))}
+                </List>
             </Toolbar>
 
         </AppBar>
